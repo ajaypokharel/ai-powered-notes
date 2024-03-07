6fcd1948507028c34af9260957580259934f60fe
Ajay Pokharel <ajaypokharel8@gmail.com>
2024-03-06 19:30:56 -0600

    [Modify] modifies API definition

--- backend/api/app.py
+++ backend/api/app.py
@@ -8,7 +8,7 @@ from transcribe import transcribe_lecture
 app = Flask(__name__)
 CORS(app) 
 
-@app.route('/api/summarize', methods=['POST'])
+@app.route('/api/link-summary', methods=['POST'])
 def summarize():
     """Summarizes a YouTube lecture given a link.
     Parameters:
@@ -28,6 +28,28 @@ def summarize():
 
     # Transcribe the audio
     transcribed_text = transcribe_lecture(audio_data)
+
+    # Summarize the transcribed text using the fine-tuned BART model
+    summary = call_summarization_model(transcribed_text)
+
+    # Return the summary as a JSON response
+    return jsonify({'summary': summary})
+
+@app.route('/api/record-summary', methods=['POST'])
+def transcribe():
+
+    # Transcribe the audio
+    """This function transcribes an audio file and summarizes the transcribed text using a fine-tuned BART model. 
+    Parameters:
+        - request (object): Flask request object containing the audio file.
+    Returns:
+        - summary (str): Summary of the transcribed text.
+    Processing Logic:
+        - Transcribe audio using request data.
+        - Summarize transcribed text using BART model.
+        - Return summary as JSON response."""
+    print(request)
+    transcribed_text = request.json.get('finalTranscript')
 
     # Summarize the transcribed text using the fine-tuned BART model
     summary = call_summarization_model(transcribed_text)
